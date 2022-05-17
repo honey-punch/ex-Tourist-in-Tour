6f548044dbfda33c55f6be2152de9fe1e67733d2
mad <mad@jangjicheol.local>
2022-05-18 08:55:54 +0900

    2

--- javascript/common.js
+++ javascript/common.js
@@ -11,9 +11,29 @@ $(document).ready(function() {
     });
 
     // img fade in & fade out
-    // $(".imglist li:gt(0)").hide();
+
+    $(".imglist li:gt(0)").hide();
     setInterval(function() {
-        $(".imglist li:first-child").fadeOut().next('li').fadeIn().end().appendTo(".imglist");
-    }, 3000);
+         $(".imglist li:first-child").fadeOut().next('li').fadeIn(500).end().appendTo(".imglist");
+     }, 3000);
+
+    
+    // img slide
+
+    // var imgs = 2;
+    // var now = 0;
+
+    // start();
+
+    // function start() {
+    //     $(".imglist>li").eq(0).siblings().animate({width: "-100%"});
+    
+    //     setInterval(function() {
+    //         now = now==imgs?0:now+=1;
+    //         $(".imglist>li").eq(now-1).animate({width: "-100%"});
+    //         $(".imglist>li").eq(now).animate({width: "100%"});},3000);
+    // }
+
+    clearInterval();
 
 });
