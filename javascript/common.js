f40d341c9af35648f38a941262437e6bd9ac2d22
mad <mad@jangjicheol.local>
2022-05-19 17:42:06 +0900

    5

--- javascript/common.js
+++ javascript/common.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     
+    // menu
+    
     $(".submenu").hide();
 
     $(".menu>li").mouseover(function() {
@@ -10,29 +12,22 @@ $(document).ready(function() {
         $(this).find(".submenu").stop().slideUp(500);
     });
 
+    // quickmenu toTop
+
+    $(".quickmenu li:last-child a").click(function() {
+        toTop();
+    });
+
+    $(".top_mob").click(function() {
+        toTop();
+    });
+
     // img fade in & fade out
 
-    // $(".imglist li:gt(0)").hide();
+    $(".imglist li:gt(0)").hide();
     setInterval(function() {
          $(".imglist li:first-child").fadeOut().next('li').fadeIn(500).end().appendTo(".imglist");
      }, 3000);
-
-    
-    // img slide
-
-    // var imgs = 2;
-    // var now = 0;
-
-    // start();
-
-    // function start() {
-    //     $(".imglist>li").eq(0).siblings().animate({width: "-100%"});
-    
-    //     setInterval(function() {
-    //         now = now==imgs?0:now+=1;
-    //         $(".imglist>li").eq(now-1).animate({width: "-100%"});
-    //         $(".imglist>li").eq(now).animate({width: "100%"});},3000);
-    // }
 
     clearInterval();
 
